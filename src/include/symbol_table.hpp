b2da9b51f90d0eb8df908b06ecf560cfd7dfcea5
bihany-harsh <harshbihany7@gmail.com>
2024-03-08 17:38:24 +0530

    symbol table defintions

--- src/include/symbol_table.hpp
+++ src/include/symbol_table.hpp
@@ -11,6 +11,14 @@ typedef enum base_data_types {
     VOID
 } base_data_type;
 
+typedef enum symbol_table_type {
+    GLOBAL,
+    FUNCTION,
+    CLASS,
+    BLOCK,
+    OTHER,
+} symbol_table_type;
+
 struct symbol_table;
 struct symbol_table_entry;
 
@@ -28,7 +36,7 @@ typedef struct class_attributes {
     vector<struct symbol_table_entry*> members; // members as well as methods should work
 } class_attr;
 
-union xattr {
+union extended_attr {
     list_attr list_attr;
     func_attr func_attr;
     class_attr class_attr;
@@ -49,33 +57,34 @@ typedef struct symbol_table_entry {
     int offset = 0;
     int decl_line = 0;
     int scope = 0;
+    // NOTE: the following fields to be set explicitly by the parser.
     bool is_list = false;
     bool is_function = false;
     bool is_class = false;
 
-    // additional attributes that might be required
-    xattr* extended_attr = nullptr;
-
     symbol_table* child_symbol_table = nullptr;
 
     symbol_table_entry(string name, base_data_type b_type, int size, int offset, int decl_line, int scope);
-    symbol_table_entry(string name, base_data_type b_type, int size, int offset, int decl_line, int scope, xattr* xattr);
+    symbol_table_entry(string name, base_data_type b_type, int size, int offset, int decl_line, int scope, extended_attr* xattr);
 } st_entry;
 
 typedef struct symbol_table {
+    symbol_table_type st_type;
     string st_name = "";
     vector<st_entry*> entries;
     symbol_table* parent = nullptr;
     int scope = 0;
-    int offset = 0;
+    // int offset = 0; // NOTE: probably not required here
+
+    // additional attributes that might be required
+    extended_attr* xattr = nullptr;
 
     symbol_table();
-    symbol_table(string st_name, symbol_table* parent);
+    symbol_table(symbol_table_type st_type, string st_name, symbol_table* parent);
     void add_entry(st_entry* entry);
     int delete_entry(string name);
 
     st_entry* get_entry(string name);
-    st_entry* get_entry(string name, int scope);
-    void set_scope(symbol_table* child_symbol_table); // adding a new scope
+    void set_scope(); // adding a new scope
 
 } symbol_table;
--- src/symbol_table.cpp
+++ src/symbol_table.cpp
@@ -6,17 +6,96 @@
 #include "include/symbol_table.hpp"
 using namespace std;
 
+extern void yyerror (char const*);
+char sym_error_string[256];
+
 symbol_table* GLOBAL_SYMBOL_TABLE;
 
 symbol_table::symbol_table() {
     
 }
 
-symbol_table::symbol_table(string st_name, symbol_table* parent) {
+symbol_table::symbol_table(symbol_table_type st_type, string st_name, symbol_table* parent) {
+    this->st_type = st_type;
     this->st_name = st_name;
     this->parent = parent;
 }
 
 void symbol_table::add_entry(st_entry* entry) {
-    
+    // TODO: decide the datatype for the function, class etc.
+    // to add a new entry in the given symbol table
+    // ERROR CASE: already present
+
+    // TODO: proper error messages
+    for (st_entry* present_entry: this->entries) {
+        if (present_entry->name == entry->name) {
+            snprintf(sym_error_string, sizeof(sym_error_string), "Reconsider identifier name. Already used on line no.: %d", present_entry->decl_line);
+            yyerror(sym_error_string);
+        }
+    }
+    if (this->st_type == FUNCTION) {
+        // check for function parameters
+        for(st_entry* param: this->xattr->func_attr.params) {
+            if (param->name == entry->name) {
+                yyerror("Identifier already a function parameter.");
+            }
+        }
+    }
+    if (this->st_type == CLASS) {
+        for (st_entry* member: this->xattr->class_attr.members) {
+            if(member->name == entry->name) {
+                //NOTE: to check for STATIC POLYMORPHISM
+                if (member->is_function && entry->is_function) {
+                    bool flag = false;
+                    for(int i = 0; i < min(member->child_symbol_table->xattr->func_attr.params.size(), entry->child_symbol_table->xattr->func_attr.params.size()); i++) {
+                        st_entry* p1 = member->child_symbol_table->xattr->func_attr.params[i];
+                        st_entry* p2 = entry->child_symbol_table->xattr->func_attr.params[i];
+                        if (p1->b_type != p2->b_type) {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (!flag) {
+                        yyerror("Function definition already exists.");
+                    }
+                } else {
+                    yyerror("Class member already exists!");
+                }
+            }
+        }
+    }
+
+    // ELSE push_back into the st_entry list
+    this->entries.push_back(entry);
+}
+
+int symbol_table::delete_entry(string name) {
+    for (int i = 0; i < this->entries.size(); i++) {
+        if(this->entries[i]->name == name) {
+            delete this->entries[i];
+            auto newEnd = remove(this->entries.begin(), this->entries.end(), nullptr);
+            this->entries.erase(newEnd, this->entries.end());
+            return 0;
+        }
+    }
+
+    return -1; // erroneous!
+}
+
+st_entry* symbol_table::get_entry(string name) {
+    symbol_table* st = this;
+    while(st) {
+        for(st_entry* entry: st->entries) {
+            if(entry->name == name) {
+                return entry;
+            }
+        }
+        st = st->parent;
+    }
+
+    return nullptr; // could not find entry
+}
+
+void symbol_table::set_scope() {
+    this->scope = this->parent->scope + 1;
 }
